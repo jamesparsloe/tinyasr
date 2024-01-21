7120ef12462245a5f45cf3a345826fe4d7078438
James Parsloe <jamesparsloe@gmail.com>
2024-01-21 13:48:51 +0000

    Tweaks

--- dataset_size.py
+++ dataset_size.py
@@ -1,14 +1,21 @@
-from datasets import load_dataset
+from datasets import load_dataset, interleave_datasets
 from itertools import islice
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-train_ds = load_dataset(
-    "mozilla-foundation/common_voice_16_1",
-    "en",
-    split="train",
-    trust_remote_code=True,
-)
+ds = []
+
+for lang in ["en", "es", "fr", "de", "it"]:
+    d = load_dataset(
+        "mozilla-foundation/common_voice_16_1",
+        lang,
+        split="train",
+        trust_remote_code=True,
+    )
+    print(f"{lang=} {len(d)} samples")
+    ds.append(d)
+
+train_ds = interleave_datasets(ds, stopping_strategy="all_exhausted")
 
 batch_size = 64
 samples = len(train_ds)
